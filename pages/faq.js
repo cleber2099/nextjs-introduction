be2c05198aa110734c15215fa7f7c9eee7a025a2
cleber <cleber.santos.2099@gmail.com>
2023-11-09 18:23:58 -0300

    first commit

---
+++ pages/faq.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function HomePage(){
+    return(
+        <div>
+            <h1> Alura Cases - Páginas de Perguntas</h1>
+
+            <Link href="/">
+                 Ir para  o Home
+            </Link>
+        </div>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Link from "next/link"
+export default function HomePage(){
+    return(
+        <div>
+            <h1> Alura Cases - Home Page</h1>
+            <Link href="/faq">
+            Ir para  o FAQ
+            </Link>
+        </div>
+    )
+}
